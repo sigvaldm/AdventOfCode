165b7466b68f1fe378e21da518d290fca536c6b0
sigvaldm <sigvald@marebakken.com>
2021-12-13 18:58:20 +0100

    Seemingly working version

--- 2021/03/b.hs
+++ 2021/03/b.hs
@@ -1,39 +1,27 @@
 import Data.List (transpose)
+import Data.Char (digitToInt)
 
 -- List of binary integers to integer
 binToInt' [] = 0
-binToInt' (x : xs) = x + 2 * binToInt' xs
+binToInt' (x : xs) = (digitToInt x) + 2 * binToInt' xs
 binToInt = binToInt'.reverse
 
--- Convert strings "001011" to 0 or 1 depending on majority count
-convertStr s
-    | 2*(countOnes s) < (length s) = 0
-    | otherwise                    = 1
-    where countOnes = length . filter (== '1')
-
-countOnes = length . filter (== '1') . head
+countOnes = length . filter ((== '1') . head)
 chooseMajority xs = if 2*(countOnes xs) >= length xs then '1' else '0'
-chooseMinority xs = if 2*(countOnes xs) < length xs  then '0' else '1'
+chooseMinority xs = if 2*(countOnes xs) >= length xs then '0' else '1'
 
 findRate :: [String] -> String -> ([String]->Char) -> String
 findRate [x] result predicate = result++x
 findRate xs result predicate = do
-    -- let numOnes = length $ filter ((=='1').head) xs
-    -- let majority = if 2*numOnes >= length xs
-    --                then '1'
-    --                else '0'
-    let majority = predicate xs
-    let ys = filter h xs where h x = head x == majority
+    let chosen = predicate xs
+    let ys = filter h xs where h x = head x == chosen
     let zs = map (drop 1) ys
-    let new_result = result ++ [majority]
+    let new_result = result ++ [chosen]
     findRate zs new_result predicate
 
 main = do
     input <- readFile "dummy"
     let lines = words input
-    -- let x = filter h lines where h x = head x == '0'
-    print $ findRate lines "" chooseMajority
-    -- print $ chooseMajority ["0101", "0000", "1001", "1111"]
-    -- print lines
-    -- print x
-    -- print $ findRate $ findRate lines
+    let oxygenRate = binToInt $ findRate lines "" chooseMajority
+    let carbRate = binToInt $ findRate lines "" chooseMinority
+    print (oxygenRate*carbRate)
