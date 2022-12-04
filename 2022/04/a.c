6ef1378417d5053021ad398f1788398c0e1815de
sigvaldm <sigvald@marebakken.com>
2022-12-04 10:36:38 +0100

    Solved day 4, part 1 in C

---
+++ 2022/04/a.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int fully_contained(const int *elf1, const int *elf2);
+
+int main(int argc, char **argv){
+
+    FILE *fp = fopen("input.txt", "r");
+
+    int elf1[2], elf2[2];
+    int fully_contained_pairs = 0;
+
+    while(fscanf(fp, "%d-%d,%d-%d\n", elf1, elf1+1, elf2, elf2+1) != EOF)
+        fully_contained_pairs += fully_contained(elf1, elf2);
+
+    printf("%d\n", fully_contained_pairs);
+
+    fclose(fp);
+    return 0;
+}
+
+int fully_contained(const int *elf1, const int *elf2){
+    if( elf1[0] >= elf2[0] && elf1[1] <= elf2[1]) return 1;
+    if( elf2[0] >= elf1[0] && elf2[1] <= elf1[1]) return 1;
+    return 0;
+}
