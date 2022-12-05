0a4c76d5d22b36d042d283d7dfffcd2951582a98
Sigvald Marholm <sigvald@marebakken.com>
2022-12-05 22:02:01 +0100

    Day 5, part 1, in Python

---
+++ 2022/05/ab.py
@@ -0,0 +1,9 @@
+import re
+stacks, moves = open('input.txt').read().split("\n\n")
+stacks = list(map(lambda a: "".join(a).strip()[:-1], zip(*stacks.splitlines())))[1::4]
+for move in moves.splitlines():
+    amount, fro, to = map(int, re.findall("\d+", move))
+    fro, to = fro-1, to-1
+    stacks[to] = stacks[fro][amount-1::-1] + stacks[to]
+    stacks[fro] = stacks[fro][amount:]
+print("".join(list(zip(*stacks))[0]))
